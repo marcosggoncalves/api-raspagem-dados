820227bdb71204b91aae624f426da911fcc60484
Marcos Lopes <marcos@dourasoft.com.br>
2020-07-14 23:33:01 -0400

    Configurando package.json

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./config/server.js');
 const port = process.env.PORT || 3001;
 
 server.listen(port, () => {
-    console.log("Servidor inicializado na porta: " + 3000);
+    console.log("Servidor inicializado na porta: " + port);
 });
