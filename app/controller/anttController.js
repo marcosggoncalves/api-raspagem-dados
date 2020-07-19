6e69ff2c5b1311e20266673de1fa0ced83ef3fa0
Marcos Lopes <marcos@dourasoft.com.br>
2020-07-18 21:40:14 -0400

    Escrevendo em arquivo json

--- app/controller/anttController.js
+++ app/controller/anttController.js
@@ -2,36 +2,53 @@ module.exports.index = (req,res)=>{
 	const request = require('request');
     const cheerio = require('cheerio');
     const moment = require("moment");
+    const fs = require('fs');
 
     moment.locale('pt-BR');
 
-    request('https://www.antt.gov.br/web/guest/noticias-e-eventos', function(error, response, html) {
-      	var $ = cheerio.load(html);
+    fs.exists('antt.json', function(exists){
+        if(exists && moment().format('mm') === "20"){ // Atualizar em 1 hora
+            fs.unlink('antt.json',(err)=>{
+                if(err) return console.log(err);
+           }); 
+        }
 
-        var results = [];
+        if(exists){
+          res.json(JSON.parse(fs.readFileSync('antt.json')));
+        }else{
+            request('https://www.antt.gov.br/web/guest/noticias-e-eventos', function(error, response, html) {
+              	var $ = cheerio.load(html);
 
-        $('.cards a').each(function(i) {
-            if(i < 4){
-                let data = $(this).find('.subtexto').eq(0).text().trim();
-                let titulo = $(this).find('.corpo h5').eq(0).text().trim();
-                let imagem = $(this).find('img').eq(0).attr('src');
-                let link = $(this).eq(0).attr('href');
+                var results = [];
 
-                results.push({
-                    titulo: titulo,
-                    link: `https://www.antt.gov.br/${link}`,
-                    imagem: imagem,
-                    data:data
-                });  
-            }          
-        });
+                $('.cards a').each(function(i) {
+                    if(i < 4){
+                        let data = $(this).find('.subtexto').eq(0).text().trim();
+                        let titulo = $(this).find('.corpo h5').eq(0).text().trim();
+                        let imagem = $(this).find('img').eq(0).attr('src');
+                        let link = $(this).eq(0).attr('href');
 
-        res.json({
-            "atualizado":moment().format('L'),
-			"success": true,
-			"data": {
-                "noticias": results
-            }
-		});
+                        results.push({
+                            titulo: titulo,
+                            link: `https://www.antt.gov.br/${link}`,
+                            imagem: imagem,
+                            data:data
+                        });  
+                    }          
+                });
+
+                let data = {
+                    "atualizado":moment().format('LLL'),
+                    "success": true,
+                    "data": {
+                        "noticias": results
+                    }
+                };
+
+                fs.writeFileSync('antt.json', JSON.stringify(data));
+
+                res.json(data);
+            });
+        }
     });
 }
--- app/controller/revistadoonibusController.js
+++ app/controller/revistadoonibusController.js
@@ -2,36 +2,53 @@ module.exports.index = (req,res)=>{
 	const request = require('request');
     const cheerio = require('cheerio');
     const moment = require("moment");
+    const fs = require('fs');
 
     moment.locale('pt-BR');
 
-    request('https://revistadoonibus.com/', function(error, response, html) {
-      	var $ = cheerio.load(html);
+    fs.exists('revista.json', function(exists){
+        if(exists && moment().format('mm') === "20"){ // Atualizar em 1 hora
+            fs.unlink('revista.json',(err)=>{
+                if(err) return console.log(err);
+           }); 
+        }
 
-        var results = [];
+        if(exists){
+          res.json(JSON.parse(fs.readFileSync('revista.json')));
+        }else{ 
+            request('https://revistadoonibus.com/', function(error, response, html) {
+              	var $ = cheerio.load(html);
 
-        $('.featured-small-items .news-item').each(function(i) {   
-             if(i < 4){
-                let data = $(this).find('.posted-date').eq(0).text().trim();
-                let titulo = $(this).find('.entry-title').eq(0).text().trim();
-                let imagem = $(this).find('img').eq(0).attr('src');
-                let link = $(this).find('.news-thumb a').eq(0).attr('href');
+                var results = [];
 
-                results.push({
-                    titulo: titulo,
-                    link: link,
-                    imagem: imagem,
-                    data:data
-                });  
-            }
-        });
+                $('.featured-small-items .news-item').each(function(i) {   
+                     if(i < 4){
+                        let data = $(this).find('.posted-date').eq(0).text().trim();
+                        let titulo = $(this).find('.entry-title').eq(0).text().trim();
+                        let imagem = $(this).find('img').eq(0).attr('src');
+                        let link = $(this).find('.news-thumb a').eq(0).attr('href');
 
-        res.json({
-            "atualizado":moment().format('L'),
-			"success": true,
-			"data": {
-				"noticias": results
-			}
-		});
+                        results.push({
+                            titulo: titulo,
+                            link: link,
+                            imagem: imagem,
+                            data:data
+                        });  
+                    }
+                });
+
+                let data = {
+                    "atualizado":moment().format('LLL'),
+                    "success": true,
+                    "data": {
+                        "noticias": results
+                    }
+                };
+
+                fs.writeFileSync('revista.json', JSON.stringify(data));
+
+                res.json(data);
+            });
+        }
     });
 }
