fe2c670d2c9930c02bc77cd5649b9538390b471d
Marcos Lopes <marcos@dourasoft.com.br>
2020-07-18 21:49:12 -0400

    Raspando noticias de onibus apenas do brasil

--- app/controller/revistadoonibusController.js
+++ app/controller/revistadoonibusController.js
@@ -16,12 +16,12 @@ module.exports.index = (req,res)=>{
         if(exists){
           res.json(JSON.parse(fs.readFileSync('revista.json')));
         }else{ 
-            request('https://revistadoonibus.com/', function(error, response, html) {
+            request('https://revistadoonibus.com/category/brasil/', function(error, response, html) {
               	var $ = cheerio.load(html);
 
                 var results = [];
 
-                $('.featured-small-items .news-item').each(function(i) {   
+                $('.col-md-8 article').each(function(i) {   
                      if(i < 4){
                         let data = $(this).find('.posted-date').eq(0).text().trim();
                         let titulo = $(this).find('.entry-title').eq(0).text().trim();
